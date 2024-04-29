5dbe4a61bec3abcdfe1ac6f9fec41fc4a79c05aa
Rixhee <rishitbansals@gmail.com>
2024-04-28 21:20:15 -0400

    Multiple scans, 3d and quiver, removed extra files

--- rotateAngle.m
+++
@@ -1,3 +0,0 @@
-function rotate = rotateAngle(theta)
-    rotate = [cos(theta) -1 * sin(theta) 0; sin(theta) cos(theta) 0; 0 0 1];
-end
--- translatePos.m
+++
@@ -1,3 +0,0 @@
-function translate = translatePos(x, y)
-    translate = [1 0 x; 0 1 y; 0 0 1];
-end
